cbc833cdb1493775f1f0bcc02ce2cd48b4632e37
LenHristova <lenhristova@gmail.com>
2018-12-15 00:10:24 +0200

    Introduce home page design

---
+++ src/Web/TwentyFirst.Web/Controllers/ArticlesController.cs
@@ -0,0 +1,25 @@
+﻿namespace TwentyFirst.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Services.DataServices.Contracts;
+
+    public class ArticlesController : BaseController
+    {
+        private readonly IArticleService articleService;
+
+        public ArticlesController(IArticleService articleService)
+        {
+            this.articleService = articleService;
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Details(string id)
+        {
+            return this.View();
+        }
+    }
+}
